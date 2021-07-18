169ab490e0ac6b2f94b65c28174a0ca434d2fd9e
Dylan <dylan.grrant@gmail.com>
2021-07-18 18:06:33 -0400

    Player class

---
+++ app/src/main/java/com/example/roulette/Player.java
@@ -0,0 +1,28 @@
+package com.example.roulette;
+
+public class Player {
+
+    private int balance;
+
+    public Player(){
+        this.balance=0;
+    }
+    public Player(int startBalance){
+        this.balance=startBalance;
+    }
+
+    public int getBalance(){
+        return this.balance;
+    }
+    public void addBalance(int amount){
+        this.balance+=amount;
+    }
+    public void takeBalance(int amount){
+        this.balance-=amount;
+    }
+
+    public void setBalance(int amount) {
+        this.balance = amount;
+    }
+
+}
