ecb269d746ef7e90c3708280992bc1261b9937e2
Dylan <dylan.grrant@gmail.com>
2021-07-18 20:48:53 -0400

    landscape

--- app/src/main/java/com/example/roulette/MainActivity.java
+++ app/src/main/java/com/example/roulette/MainActivity.java
@@ -73,4 +73,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
