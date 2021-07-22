a165edb2b91808b32e09b56de1d0650e0abc81f0
Dylan <dylan.grrant@gmail.com>
2021-07-22 15:20:05 -0400

    fix

--- app/src/main/java/com/example/roulette/MainActivity.java
+++ app/src/main/java/com/example/roulette/MainActivity.java
@@ -42,8 +42,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
         totalBet=0;
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
@@ -54,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         betcount = (TextView) findViewById(R.id.totalbet);
+
 
 
 
