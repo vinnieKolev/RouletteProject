25ddd21827be6178da2777c15c998655e4ec1090
Dylan <dylan.grrant@gmail.com>
2021-07-29 15:39:58 -0400

    dsfg

--- app/src/main/java/com/example/roulette/Payouts.java
+++ app/src/main/java/com/example/roulette/Payouts.java
@@ -5,20 +5,41 @@ public class Payouts {
     // This is a Utility class. No instances.
     private Payouts() {}
 
-    public static int straight(int bet) {
+
+    public static int straightBet(int bet) {
         return bet + (bet * 35);
     }
 
-    public static int green(int bet) {
-        return bet + (bet * 35);
-    }
-
-    public static int twoConsecutive(int bet) {
+    public static int splitBet(int bet) {
         return bet + (bet * 17);
     }
 
-    public static int threeConsecutive(int bet) {
+    public static int streetBet(int bet) {
         return bet + (bet * 11);
+    }
+
+    public static int trioBet(int bet) {
+        return bet + (bet * 11);
+    }
+
+    public static int cornerBet(int bet) {
+        return bet + (bet * 8);
+    }
+
+    public static int topLineBet(int bet) {
+        return bet + (bet * 6);
+    }
+
+    public static int doubleStreetBet(int bet) {
+        return bet + (bet * 5);
+    }
+
+    public static int columnBet(int bet) {
+        return bet + (bet * 2);
+    }
+
+    public static int dozenBet(int bet) {
+        return bet + (bet * 2);
     }
 
     public static int oddEven(int bet) {
